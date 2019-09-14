a56b4a634c61842a905e8f106b687e64d5e88e34
Omar Gonzalez <gonzalezoe82@gmail.com>
2019-09-14 21:33:58 +0900

    can now render project routes

--- routes/project.js
+++ routes/project.js
@@ -4,9 +4,28 @@ const  { data } = require('../data/projectData.json');
 const  { projects } = data;
 
 router.get('/projects/:id', (req, res) => {
-    res.render('projects', {
-       projects: projects[req.params.id]
-    });
+    const { id } = req.params;
+    const { project_name } = projects[id];
+    const { description } = projects[id];
+    const { technologies } = projects[id];
+    const { live_link } = projects[id];
+    const { github_link } = projects[id];
+    const { image_urls } = projects[id];
+    
+    const templateData = {
+        id,
+        project_name,
+        description,
+        technologies,
+        live_link,
+        github_link,
+        image_urls,
+        image_urls
+    }
+    res.render('project', templateData);
+
 });
 
+
 module.exports = router;
+
